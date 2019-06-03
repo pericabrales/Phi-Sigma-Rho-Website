f15adfa2920bc3c1b16a8eceb7882749cde0b263
Peri Cabrales <cabralep@flip2.engr.oregonstate.edu>
2019-06-02 18:02:49 -0700

    fixed the server I think

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+var express = require('express');
+var app = express();
 var path = require('path');
 var port = process.env.PORT || 3010;
+
+app.use(express.static('public'));
 
 app.listen(port, function() {
 	console.log("== The Server is listening on port: ", port);

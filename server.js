79de1516310661cda84c750e71744c46736ed1cd
Katelynn Thorn <katelynnthorn@Katelynns-MacBook.local>
2019-06-02 19:48:23 -0700

    Templates for server.js

--- server.js
+++ server.js
@@ -1,45 +1,31 @@
-var express = require('express');
-var app = express();
 var path = require('path');
+var express = require('express');
 var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
+var twitNumber;
 
-var http = require('http');
-var fs = require('fs');
-var server = http.createServer(requestHandler);
-var url = require('url');
-//var port = process.env.PORT || 3010;
+var app = express();
+var port = process.env.PORT || 3000;
 
 
-//var app = express();
-//var port = process.env.PORT || 3000;
-
-/*app.use(express.static('public'));
-
-app.listen(port, function() {
-	console.log("== The Server is listening on port: ", port);
-});
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
-*/
 
-function requestHandler(req, res){
-		filename = 'public/homepage.html';
-	fs.readFile(filename, function(err, data) {
-		if(err){
-			res.statusCode = 404;
-			var error = fs.readFileSync('public/404.html');
-			res.write(error);
-			return res.end();
-		}
-                res.statusCode = 200;
-                res.write(data);
-                return res.end();
-        });
+app.get('/', function(req, res, next){
+    res.status(200).render('homepage', {
+      header: true
+    });
+});
 
-}
+app.get('*', function (req, res) {
+  res.status(404).render('404Page');
+});
 
-server.listen(3000);
+app.listen(port, function (err) {
+  if(err){
+    throw err;
+  }
+  console.log("== Server is listening on port", port);
+});

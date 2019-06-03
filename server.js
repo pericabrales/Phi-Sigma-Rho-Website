2020d27c85c0725cca582c74ae4ce1c804886b95
Katelynn Thorn <katelynnthorn@Katelynns-MacBook.local>
2019-06-02 18:14:42 -0700

    Server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
+var app = express();
 var port = process.env.PORT || 3010;
 
 app.listen(port, function() {

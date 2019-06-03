41555a1ed17a067b85afc8226e3a98aab757d690
Peri Cabrales <cabralep@flip2.engr.oregonstate.edu>
2019-06-02 18:20:46 -0700

    changed the merge

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
-var app = express();
 var port = process.env.PORT || 3010;
 
 app.use(express.static('public'));

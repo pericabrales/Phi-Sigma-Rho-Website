1548291a03c7cade9c709f794f2352f887c8a271
Katelynn Thorn <katelynnthorn@Katelynns-MacBook.local>
2019-06-02 18:40:21 -0700

    Changes to server.js

--- server.js
+++ server.js
@@ -5,11 +5,40 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
-var app = express();
-var port = process.env.PORT || 3010;
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(requestHandler);
+var url = require('url');
 
-app.use(express.static('public'));
+//var app = express();
+//var port = process.env.PORT || 3000;
+
+/*app.use(express.static('public'));
 
 app.listen(port, function() {
 	console.log("== The Server is listening on port: ", port);
 });
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+*/
+function requestHandler(req, res){
+		filename = 'public/homepage.html';
+//	console.log("Reading this only once?");
+	fs.readFile(filename, function(err, data) {
+		if(err){
+			res.statusCode = 404;
+			var error = fs.readFileSync('public/404.html');
+			res.write(error);
+			return res.end();
+		}
+                res.statusCode = 200;
+                res.write(data);
+                return res.end();
+        });
+
+}
+
+server.listen(3000);

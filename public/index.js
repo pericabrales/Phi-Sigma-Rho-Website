c2d2c0638c9d1223820c8a5b5ca599680e585f17
Katelynn Thorn <katelynnthorn@Katelynns-MacBook.local>
2019-06-04 21:05:32 -0700

    Calendar event handlebars

--- public/index.js
+++ public/index.js
@@ -1,23 +1,54 @@
-  var cook=document.querySelector("#cookies");
+  /*var cook=document.querySelector("#cookies");
   if(cook){
     cook.addEventListener("click", function(){
   function cookiesCreate(){
     var modal = document.querySelector("#cookies-open-modal");
     var backdrop = document.querySelector("#cookies-modal-backdrop");
     modal.style.display = "block"; /*show if clicked*/
-    backdrop.style.display= "block"; /*show if clicked*/
+//    backdrop.style.display= "block"; /*show if clicked*/
+//  }
+//  var modal = document.querySelector("#open-modal");
+
+  var modalContext = {
+    eventTitle: "eventTitle",
+    when: "when",
+    where: "where",
+    what: "what"
+  };
+
+  var modalHTML = Handlebars.templates.modal(modalContext);
+  (document.body).insertAdjacentHTML('beforeend', modalHTML);
+  var modal = document.querySelector("#open-modal");
+  var backdrop = document.querySelector("#modal-backdrop");
+  var calendarDays = document.getElementsByClassName("event");
+
+
+  for(var i = 0; i < calendarDays.length; i++){
+    calendarDays[i].addEventListener('click', function calendarDaysFunction(){
+
+        modal.classList.remove('hidden');
+        backdrop.classList.remove('hidden');
+    });
   }
 
-  /*if cancel button clicked or no input, then close the modal*/
 
+  var modalCloseButton = document.querySelector("#modal-close-button");
+  modalCloseButton.addEventListener('click', function closeModal(){
+    modal.classList.add('hidden');
+    backdrop.classList.add('hidden');
+  });
+
+
+  /*if cancel button clicked or no input, then close the modal*/
+/*
   function closeModal(){
     var modal = document.querySelector("#cookies-open-modal");
     var backdrop = document.querySelector("#cookies-modal-backdrop");
     console.log("x button clicked");
     modal.style.display = "none"; /*do not show if clicked*/
-    backdrop.style.display= "none"; /*do not show if clicked*/
-  }
-
+//    backdrop.style.display= "none"; /*do not show if clicked*/
+//  }
+/*
   var close= document.querySelector(".modal-close-button");
     if(close){
       close.addEventListener("click", function(){
@@ -33,3 +64,4 @@
   }
 });
 }
+*/
